4306049b426138f71a15519c58c9786a9fcd5c3f
Hiba-Farah <farahhiba068@gmail.com>
2026-05-15 01:36:15 +0000

    Fix terraform use existing network

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,48 +1,34 @@
-# Réseau interne
-resource "openstack_networking_network_v2" "network" {
-  name           = "autoheal-network"
-  admin_state_up = true
-}
-
-resource "openstack_networking_subnet_v2" "subnet" {
-  name       = "autoheal-subnet"
-  network_id = openstack_networking_network_v2.network.id
-  cidr       ="192.168.100.0/24"
-  ip_version = 4
+# Utiliser le réseau existant
+data "openstack_networking_network_v2" "network" {
+  name = "autoheal-network"
 }
 
 # VM web-1
 resource "openstack_compute_instance_v2" "vm_web_1" {
-  name            = "vm-web-1"
-  image_name      = "cirros"
-  flavor_name     = "m1.tiny"
+  name        = "vm-web-1"
+  image_name  = "cirros"
+  flavor_name = "m1.tiny"
   network {
-    name = openstack_networking_network_v2.network.name
+    uuid = data.openstack_networking_network_v2.network.id
   }
 }
 
 # VM web-2
 resource "openstack_compute_instance_v2" "vm_web_2" {
-  name            = "vm-web-2"
-  image_name      = "cirros"
-  flavor_name     = "m1.tiny"
+  name        = "vm-web-2"
+  image_name  = "cirros"
+  flavor_name = "m1.tiny"
   network {
-    name = openstack_networking_network_v2.network.name
+    uuid = data.openstack_networking_network_v2.network.id
   }
 }
 
 # VM monitor
 resource "openstack_compute_instance_v2" "vm_monitor" {
-  name            = "vm-monitor"
-  image_name      = "cirros"
-  flavor_name     = "m1.tiny"
+  name        = "vm-monitor"
+  image_name  = "cirros"
+  flavor_name = "m1.tiny"
   network {
-    name = openstack_networking_network_v2.network.name
+    uuid = data.openstack_networking_network_v2.network.id
   }
 }
-# Pipeline CI/CD test
-# test CI/CD
-# test CI/CD
-# CI/CD test
-# trigger
-# test
